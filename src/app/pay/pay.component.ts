f0f63ef5766ac0b1057b7cd5369d67f49e5380cc
esaleh15 <elisaleh1@hotmail.co.uk>
2024-04-16 00:08:39 +0300

    add the pay api requests

--- src/app/pay/pay.component.ts
+++ src/app/pay/pay.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Utils } from '../utils/utils'; 
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-pay',
@@ -7,8 +9,27 @@ import { Component } from '@angular/core';
 })
 export class PayComponent {
 
-  constructor() {
+  constructor(private utils: Utils, private http: HttpClient) {
     console.log('Loading pageContext: Pay')
    }
+
+  payNow() {
+    let body = {
+      "customerId": "dummy",
+      "action": "MAKE_PAYMENT",
+      "customerSessionId": localStorage.getItem("cdSetCsid"),
+      "activityType": "LOGIN",
+      "uuid": this.utils.randomString(10, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+      "brand": "SD",
+      "solution": "ATO",
+      "iam": "demo@demo.com"
+    }
+    this.http.post('https://hooks.zapier.com/hooks/catch/1888053/bgwofce', JSON.stringify(body))
+            .subscribe( (data) => {
+              console.log('Paid Successfully', data)
+    }, (error) => {
+      console.log('Error' , JSON.stringify(error))
+    })
+  }
 
 }
