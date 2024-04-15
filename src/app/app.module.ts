254b76b5759b7dbbc6db55f5a41db769ab726b89
esaleh15 <elisaleh1@hotmail.co.uk>
2024-04-15 23:59:36 +0300

    add init api request

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 
 import { AppRoutingModule } from './app-routing.module';
@@ -20,7 +21,8 @@ import { PayComponent } from './pay/pay.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    ReactiveFormsModule
+    ReactiveFormsModule,
+    HttpClientModule
   ],
   providers: [Utils],
   bootstrap: [AppComponent]
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -2,6 +2,7 @@ import { Component  } from '@angular/core';
 import { NavigationExtras, Router } from '@angular/router';
 import { FormBuilder, ReactiveFormsModule, FormGroup } from '@angular/forms';
 import { Utils } from '../utils/utils'; 
+import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -12,7 +13,7 @@ export class LoginComponent {
   form: FormGroup;
 
     constructor(
-      private formBuilder: FormBuilder, private utils: Utils, private router: Router) {
+      private formBuilder: FormBuilder, private utils: Utils, private router: Router, private http: HttpClient) {
         this.form = this.formBuilder.group({
           name: '',
         })
@@ -29,5 +30,25 @@ export class LoginComponent {
   onSubmit() {
     this.utils.setCustomerSessionId(this.utils.randomString(6, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'))
     this.router.navigate(['pay']);
+    this.init();
+  }
+
+  init() {
+    let body = {
+      "customerId": "dummy",
+      "action": "init",
+      "customerSessionId": localStorage.getItem("cdSetCsid"),
+      "activityType": "LOGIN",
+      "uuid": this.utils.randomString(10, '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'),
+      "brand": "SD",
+      "solution": "ATO",
+      "iam": "demo@demo.com"
+    }
+    this.http.post('https://hooks.zapier.com/hooks/catch/1888053/bgwofce', JSON.stringify(body))
+            .subscribe( (data) => {
+              console.log('Success', data)
+    }, (error) => {
+      console.log('Error' , JSON.stringify(error))
+    })
   }
 }
